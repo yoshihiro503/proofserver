7d3982fa7fbfa386e9c959373e37fe86b7dc9137
KeitaSaitou <kei@heimdal.(none)>
2012-06-10 01:56:55 +0900

    Google Adsenseの一覧を追加した

--- src/ad.v
+++ src/ad.v
@@ -1,5 +1,5 @@
 (**
- adsenseのコードを張る。
+ adsenseのコード一覧
  *)
 
 (** * 横長の言葉だけのやつ
@@ -15,4 +15,290 @@ google_ad_height = 15;
 <script type="text/javascript"
 src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 </script>#
+
+****
+<h1>レクタングル中</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* レクタングル中 */
+google_ad_slot = "6206221756";
+google_ad_width = 300;
+google_ad_height = 250;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>レクタングル大</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* レクタングル大 */
+google_ad_slot = "0868627798";
+google_ad_width = 336;
+google_ad_height = 280;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>ビッグバナー</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* ビッグバナー */
+google_ad_slot = "8206154912";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>ワイドスカイクレイパー</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* ワイドスカイクレイパー */
+google_ad_slot = "9854096047";
+google_ad_width = 160;
+google_ad_height = 600;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>モバイルバナー</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* モバイルバナー */
+google_ad_slot = "8534331070";
+google_ad_width = 320;
+google_ad_height = 50;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>バナー</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* バナー */
+google_ad_slot = "1391833869";
+google_ad_width = 468;
+google_ad_height = 60;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>ハーフバナー</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* ハーフバナー */
+google_ad_slot = "7592366117";
+google_ad_width = 234;
+google_ad_height = 60;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>スカイクレイパー</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* スカイスクレイパー */
+google_ad_slot = "3217020193";
+google_ad_width = 120;
+google_ad_height = 600;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>縦長バナー</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長バナー */
+google_ad_slot = "6327993331";
+google_ad_width = 120;
+google_ad_height = 240;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>スクエア</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* スクエア */
+google_ad_slot = "3010107055";
+google_ad_width = 250;
+google_ad_height = 250;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>スクエア小</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* スクエア小 */
+google_ad_slot = "0030438231";
+google_ad_width = 200;
+google_ad_height = 200;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>レクタングル小</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* レクタングル小 */
+google_ad_slot = "2210614994";
+google_ad_width = 180;
+google_ad_height = 150;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>ボタン</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* ボタン */
+google_ad_slot = "5085570389";
+google_ad_width = 125;
+google_ad_height = 125;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>横長大</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 横長大 */
+google_ad_slot = "8255876190";
+google_ad_width = 728;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>横長中</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 横長中 */
+google_ad_slot = "5737102276";
+google_ad_width = 468;
+google_ad_height = 15;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>横長特大</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長特大 */
+google_ad_slot = "7848838958";
+google_ad_width = 200;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>縦長大</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長大 */
+google_ad_slot = "8082059202";
+google_ad_width = 180;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>縦長中</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長中 */
+google_ad_slot = "1404298372";
+google_ad_width = 160;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+****
+<h1>縦長小</h1>
+
+<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長小 */
+google_ad_slot = "3519140504";
+google_ad_width = 120;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
  *)
