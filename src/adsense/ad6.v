253bf6e15e807b68452671c54a877df759f644f6
KeitaSaitou <kei@heimdal.(none)>
2012-06-10 19:34:27 +0900

    Google Adsenseの例をサブディレクトリadsenseに移動した

---
+++ src/adsense/ad6.v
@@ -0,0 +1,44 @@
+(**
+ * adsenseのコード一覧6
+ *)
+
+(** * 横長特大
+#<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長特大 */
+google_ad_slot = "7848838958";
+google_ad_width = 200;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>#
+
+* 縦長大
+#<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長大 */
+google_ad_slot = "8082059202";
+google_ad_width = 180;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>#
+
+* 縦長中
+#<script type="text/javascript"><!--
+google_ad_client = "ca-pub-3545276391244796";
+/* 縦長中 */
+google_ad_slot = "1404298372";
+google_ad_width = 160;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>#
+
+ *)
